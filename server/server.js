cc5d664a475ad6824637688f12be663d0c39f736
Brandon Antonelli <bantonelli07@gmail.com>
2016-12-29 21:48:01 -0500

    - Install express and body-parser packages - Add POST /todos route for todo creation

--- server/server.js
+++ server/server.js
@@ -1,3 +1,36 @@
+// Lib imports 
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Local imports 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
+// Constants
+const PORT = 3000;
+
+var app = express();
+// Middleware config (pass functions to app.use())
+app.use(bodyParser.json());
+
+// Resource creation endpoint: POST /todos
+app.post('/todos', (req, res) => {
+    // Get content from body using body parser
+    console.log(req.body);
+    var todo = new Todo({
+        text: req.body.text
+    });
+
+    todo.save().then((result) => {
+        res.status(200).send(result);
+    }).catch((error) => {
+        res.status(400).send(error);
+    });
+});
+
+// Access resource endpoint: GET /todos 
+
+app.listen(3000, () => {
+    console.log(`Started on port ${PORT}`);
+});
